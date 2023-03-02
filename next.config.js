b0100882e6e4c613ac1dad18311b5c93f8a0d02a
Htet Lin Maung <jsthtet96@gmail.com>
2023-03-02 19:12:02 +0630

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: "/scrapperweb",
+  rewrites() {
+    return [
+      {
+        source: "/scrapperweb/_next/:path*",
+        destination: "/_next/:path*",
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
